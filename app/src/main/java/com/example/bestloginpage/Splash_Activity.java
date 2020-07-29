90ad321b2488f9b8382a17fb97dd518a21816bad
TheChiragRathod <crathod116@rku.ac.in>
2020-07-29 22:33:13 +0530

    I Have Aded Splash Screen On Login Page Design.

---
+++ app/src/main/java/com/example/bestloginpage/Splash_Activity.java
@@ -0,0 +1,29 @@
+package com.example.bestloginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Activity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT=2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent =new Intent(Splash_Activity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+
+    }
+}
